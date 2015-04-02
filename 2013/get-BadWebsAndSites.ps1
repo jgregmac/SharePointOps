f3cab4ac282b9e69129581eb248e2f6e3934d149
J. Greg Mackinnon <jgreg@outlook.com>
2015-04-02 13:37:20 -0400

    Clarified code in Get-BadWebsAndSites.

--- 2013/get-BadWebsAndSites.ps1
+++ 2013/get-BadWebsAndSites.ps1
@@ -1,25 +1,36 @@
 # Bad webs and sites in this context are webs that were created using templates
-# that are not supported under SharePoint 2013.﻿
-$waName = "https://spwinauth.uvm.edu"
-$outDir = "C:\local\temp\"
-
+# that are not supported under SharePoint 2013.
 set-psdebug -Strict
+
+# Web Application to search:
+[string]$waName = "https://spwinauth.uvm.edu"
+# Output file paths:
+[string]$outDir = "C:\local\temp\"
+[string]$allWebs = $outDir + 'allWebs.csv'
+[string]$badWebs = $outdir + 'badWebs.csv'
+[string]$badSites = $outDir + 'badSites.txt'
+
 Add-PSSnapin Microsoft.SharePoint.Powershell -ErrorAction SilentlyContinue
 $Sites = Get-SPSite -WebApplication $waName -Limit All
 
 #Enumerate all webs and their template types:
-[string] $allWebs = $outDir + 'allWebsWithTemplates.csv'
-$Sites | % {$_.allWebs} | % {
-  [string]$( $_.webTemplate + ',' + $_.webTemplateID + ',' + $_.Url.ToString() `
-    + ',"' + $_.site.lastContentModifiedDate.ToShortDateString() + '"' )
-  } > $allWebs
+foreach ($site in $sites) {
+    $webs = @()
+    $webs += $site.allwebs
+    foreach ($web in $webs) {
+        [string]$out = $web.webTemplate + ',' ` 
+            + $web.webTemplateID + ',' `
+            + $web.Url.ToString() + ',' `
+            + $site.contentdatabase.name + ',"' `
+            + $site.lastContentModifiedDate.ToShortDateString() + '"'
+        $out | out-file -FilePath $allWebs -Force
+    }
+}
 
 #Find Webs using unsupported templates:
-[string] $badWebs = $outdir + 'badWebsWithTemplates.csv'
 get-content $allWebs | select-string -NotMatch `
   '^STS|^WIKI|^MPS|^SGS|^BLOG|^,90' > $badWebs
 
 #Find Sites containing bad webs:
-[string] $badSites = $outDir + 'badSites.txt'
 Get-Content $badWebs | % {$_.split(',') | Select-Object -index 2} `
   | % {get-spweb -Identity $_} | % {$_.site} | Sort-Object -Unique > $badSites
