3dbb29b4205e1016a62de47992fc3c437d179964
jgregmac <jgreg@outlook.com>
2015-04-03 23:59:02 -0400

    Get-SPIncompatibleSites.ps1 - abolished slow and unreliable array-of-array  and array-of-objects comparisons, now much faster.  Still getting too  many "bad sites" reported.  Probably need to exclude "ReportServer"  feature from reporting.

--- 2013/Get-SPIncompatibleSites.ps1
+++ 2013/Get-SPIncompatibleSites.ps1
@@ -11,13 +11,14 @@ set-psdebug -Strict
 [string]$badWebsFile = $outdir + 'badWebs.csv'
 [string]$badSitesFile = $outDir + 'badSites.txt'
 
-#Initialize log file:
+# Initialize log file:
 if (Test-Path $allWebsFile) {Remove-Item $allWebsFile -Force -Confirm:$false}
 [string[]]$allWebs = @()
-#Add header row for CSV:
-[string]$out = 'template,templateID,WebUrl,SiteUrl,SiteAdmins,contentDB,webLastModified'
-$out | out-file -FilePath $allWebsFile -Append
+# Add header row for CSV:
+#[string]$out = 'template,templateID,WebUrl,SiteUrl,SiteAdmins,contentDB,webLastModified'
+#$out | out-file -FilePath $allWebsFile -Append
 
+# Load SharePoint PowerShell CmdLets:
 Add-PSSnapin Microsoft.SharePoint.Powershell -ErrorAction SilentlyContinue
 
 function Test-SPFeatures {
@@ -40,15 +41,15 @@ function Test-SPFeatures {
         write-verbose $out
         [bool]$Matched = $false
         foreach ($src in $testArray){
-            if ($src[0] -eq $fea) {
-                $out = "    Source-only feature found: " + $src[1]
+            if ($src -eq $fea) {
+                $out = "    Source-only feature found: " + $src
                 write-verbose $out
                 [bool]$Matched = $true
                 [bool]$Excluded = $false
                 $out = "    Matched boolean set to: " + $Matched
                 write-debug $out
                 foreach ($exclude in $excludeArray) {
-                    if ($exclude -eq $src[0]) {
+                    if ($exclude -eq $src) {
                         write-verbose "        Feature is excluded from reporting.  Skipping."
                         $Excluded = $true
                         $out = "        Excluded boolean set to: " + $Excluded
@@ -58,7 +59,7 @@ function Test-SPFeatures {
                 if (-not $Excluded) {
                     write-verbose "        Feature is not excluded.  Reporting..."
                     # Cast the returned site info to an array:
-                    $badFeas += ,@($src)
+                    $badFeas += $src
                 }
             } 
         }
@@ -71,31 +72,68 @@ function Test-SPFeatures {
     }
 }
 
+function New-BadSiteReport {
+    [cmdletBinding()]
+    param(
+        $web,
+        [string[]]$badFeatNames
+    )
+    if ($web.IsRootWeb) {
+        [string]$rootWeb = $web.Url
+    } else {
+        [string]$rootWeb = $web.Site.Url
+    }
+    [array]$admins = @()
+    $admins += $web.SiteAdministrators | % {$_.UserLogin}
+    $properties = @{
+        'WebTemplate'     = $web.webTemplate;
+        #'webTemplateId'   = $web.webTemplateID; 
+        'WebUrl'          = $web.Url;
+        'SiteUrl'         = $rootWeb;
+        'Admins'          = $admins;
+        'Owner'           = $web.site.owner.userLogin;
+        'BadFeatures'     = $badFeatNames;
+        'ContentDatabase' = $web.site.contentdatabase.name;
+        'LastModified'    = $web.lastItemModifiedDate.ToShortDateString()
+    }
+    $web.Dispose()
+    $object = New-Object -TypeName PSObject -Prop $properties
+    return $object
+}
+
 #Collect all FeatureIDs in the farm:
 $srcFeatures = @()
 $srcFeatures += Get-SPFeature | Select-Object -Property Id,DisplayName,Scope
 
-#[array[]]$srcFeasNrm = @() #Normalized version of $srcFeas
-#$srcFeasNrm | %{$src2 += ,@($_.id.tostring(),$_.displayname,$_.scope.tostring())}
+[array]$nrmSrcFeatures = @()
+foreach ($src in $srcFeatures) {
+    $properties = @{'id'=$src.id.toString();
+                    'displayName'=$src.displayName;
+                    'scope'=$src.scope.toString()}
+    $object = New-Object –TypeName PSObject –Prop $properties
+    $nrmSrcFeatures += $object
+}
+remove-variable srcFeatures
 
-#Add logic to load featureIDs in the farm to which we will migrate
-#Essentially, we will do the same step as above on the destination Farm, export to file, and have this script import the the data to "destFeatures"
+# Extract the feature IDs from Get-SPFeatures in string format:
+[string[]]$srcs = @()
+$srcs += $nrmSrcFeatures | % {$_.id} 
+
+# Load featureIDs from the farm to which we will migrate
+# Data comes from the Get-SPAllFeatures.ps1 script, run on the destination farm:
 [array]$dstFeatures = Import-Csv c:\local\temp\2013Features.csv
+[string[]]$dsts = @()
+$dsts += $dstFeatures | % {$_.id} 
 
-# Let's get just the feature IDs. These are object type GUID, so need to convert to string for comparison:
-[array[]]$s = @()
-$s += $srcFeatures | %{,@($_.id.toString(),$_.displayName,$_.scope.toString())}
-# Destination IDs already are string format:
-[array]$d = @()
-$d += $dstFeatures | %{,@($_.id,$_.displayName,$_.scope)}
-remove-variable srcFeatures,dstFeatures
+remove-variable dstFeatures
 
 #Now collect the features that are present only in 2010 farm:
-[array]$srcOnly = Compare-Object $s $d | ? {$_.sideIndicator -eq '<='} | % {,@($_.inputobject)}
-remove-variable s,d
+[array]$srcOnly = Compare-Object $srcs $dsts | ? {$_.sideIndicator -eq '<='} | % {,@($_.inputobject)}
+
+remove-variable srcs,dsts
 
 #Office WebApps feature IDs... these will be excluded from reporting:
-$oWebApps = @(`
+[string[]]$okFeatures = @(`
     '8dfaf93d-e23c-4471-9347-07368668ddaf',`
     '893627d9-b5ef-482d-a3bf-2a605175ac36',`
     '738250ba-9327-4dc0-813a-a76928ba1842',`
@@ -106,37 +144,51 @@ $oWebApps = @(`
     '5709298b-1876-4686-b257-f101a923f58d'`
 )
 
+#Hashtable for Template ID to Name lookups:
+$srcOnlyHash = @{}
+foreach ($obj in $nrmSrcFeatures) {
+    foreach ($src in $srcOnly) {
+        if ($obj.id -eq $src) {
+            $srcOnlyHash.add($obj.id,$obj.displayName)
+        }
+    }
+}
+
 # What are the names of these features?
-$srcOnly | %{$_[1]} | sort
+Write-host 'Bad Features:' -ForegroundColor DarkMagenta
+$srcOnly | %{$srcOnlyHash.$($_)} | sort
+write-host
+write-host 'Not so bad features:' -ForegroundColor Green
+$okFeatures | %{$srcOnlyHash.$($_)} | sort
 
 Write-Host "Collecting all sites..." -ForegroundColor Cyan
-#[array]$Sites = Get-SPSite -WebApplication $webApplication -Limit All
-[array]$sites = get-spsite https://sharepoint.uvm.edu/sites/FDC
+[array]$Sites = Get-SPSite -WebApplication $webApplication -Limit All
+#[array]$sites = get-spsite https://sharepoint.uvm.edu/sites/FDC
 
-[array[]]$badSites2 = @()
+#[array[]]$badSites2 = @()
 [array[]]$badWebs2 = @()
 
 Write-host "Finding sites and webs that use bad features..."
 foreach ($site in $sites) {
-    #Add logic to this loop that will flag any site that uses features that are not in the destination farm.
-    #Problem here is that if only one bad feature is discovered, the result is an array of three items, instead 
-    # of an array of one array with three items.  Must Fix!
-    #maybe stop trying to manage this array of arrays.  It is needlessly confusing.
-    $badSites2 += Test-SPFeatures -PSObj $site -testArray $srcOnly -excludeArray $oWebApps -verbose
-    #if ($badFeas.count -gt 0) {
-    #    $badSites2 += ,@($site.url,$badFeas)
-    #}
+    [string[]]$badFeatIds = @()
+    $badFeatIds += Test-SPFeatures -PSObj $site -testArray $srcOnly -excludeArray $okFeatures #-verbose
+    [string[]]$badFeatNames = @()
+    $badFeatNames += $badFeatIds | %{$srcOnlyHash.$($_)}
     $webs = @()
     $webs += $site.allwebs
     foreach ($web in $webs) {
-        $badWebs2 += Test-SPFeatures -PSObj $web -testArray $srcOnly -excludeArray $oWebApps -verbose
+        $badFeatIds += Test-SPFeatures -PSObj $web -testArray $srcOnly -excludeArray $okFeatures #-verbose
+        # Don't re-initialize the array... we will aggregate site and web features together.
+        #[string[]]$badFeatNames = @()
+        $badFeatNames += $badFeatIds | %{$srcOnlyHash.$($_)}
+        $badFeatNames = $badFeatNames | Sort -Unique
+        $badWebs2 += New-BadSiteReport -web $web -badFeatNames $badFeatNames
         $web.Dispose()
     }
     $site.Dispose()
 }
 
-
-write-host "Gathering information about all webs..." -ForegroundColor Cyan
+<#write-host "Gathering information about all webs..." -ForegroundColor Cyan
 foreach ($site in $sites) {
     #Add logic to this loop that will flag any site that uses features that are not in the destination farm.
     $webs = @()
@@ -177,3 +229,4 @@ foreach ($entry in $badWebs) {
     $badSites += $out
 }
 $badSites | Sort-Object -Unique > $badSitesFile
+#>
