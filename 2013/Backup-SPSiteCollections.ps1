93c713dcbc6a3e1c16008bbf22090203bb12859d
jgregmac <jgreg@outlook.com>
2015-04-09 13:46:03 -0400

    First working Backup-SPSiteCollections.ps1 - ported from 2010 script, now uses 'Backup-SPSite' instead of 'stsadm -o backupsite'. Maybe we should use Export-SPSite instead?

--- 2013/Backup-SPSiteCollections.ps1
+++ 2013/Backup-SPSiteCollections.ps1
@@ -1,9 +1,8 @@
 <#
-.NAME
-   sp_site_backup.ps1
-
 .SYNOPSIS
-   Creates discrete backups for sharepoint sites
+   Creates discrete backups for all SharePoint Site Collections in a specified 
+   SharePoint web application.  Optionally, backups can be limited to sites 
+   that have been modified within a specified period of time.
 
 .DESCRIPTION
    The script is intended to perform nightly backups of sharepoint
@@ -12,7 +11,7 @@
    to set and remove lock information. It uses STSADM to perform
    the actual backup.
 
-.PARAMETER SiteUrl (required)
+.PARAMETER webApplication (required)
    URL to the SharePoint WebApplication
    
 .PARAMETER Destination (required)
@@ -24,77 +23,40 @@
    interval (days + hours) will be backed up. If neither parameter
    is specified, all site collections will be backed up.
 
+   Multiple intervals are summed: -days 1 -hours 12 = 36 hours
+
+.PARAMETER Limit
+   Used for testing.  If specified, the number of site collections backed up will be limited to this value.
+
 .PARAMETER WhatIf
    If specified, no backups will be performed. Log and output will show
    actions that would have been taken.
-   
+
+.EXAMPLE
+  PS C:\>.\Backup-SPSiteCollections.ps1 -webApplication https://sharepoint.mydomain.com -destination E:\backup -hours 25
+
+  Backup all sites in the web application at https://sharepoint.mydomain.com that have been modified in the last 25 hours.
+
 .NOTES
-   Author: Geoffrey Duke
+   Original Author (2007): Geoffrey Duke
+   Revisions by (2010-2013): J. Greg Mackinnon
 #>   
 [cmdletBinding()]
 param (
-
-    # URL to the Sharepoint site root
-    [string] $siteurl,
-
-    # path in which to place backup files, logs
-    [string] $destination,
-
-    # Backup sites modified within the interval specified
-    # (multiple intervals are summed: -days 1 -hours 12 = 36 hours
-    [int] $days,
-    [int] $hours,
-
-    # WhatIf
+    [parameter(Mandatory=$true)][string]$webApplication,
+    [parameter(Mandatory=$true)][string]$destination,
+    [int]$days,
+    [int]$hours,
+    [int]$limit = 0,
     [switch] $WhatIf
 )
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-#                      Script configuration
+#                    Begin Script configuration
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 set-psdebug -strict
 
-function USAGE {
-    $out = @"Usage:
-sharepoint_backup.ps1 [-siteurl] <URL> [-destination] <PATH> [-days <INT>] [-hours <INT>] [-whatif]
-
-E.G:
- Back up all sites that have been modified in the last 25 hours:
- sharepoint_backup.ps1 https://sharepoint.uvm.edu -destination E:\backup -hours 25
-
-"@
-    return $out 
-}
-
-
-if ( -not ( $siteurl -and $destination ) ) {
-    usage
-    exit
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Additional Script configuration and setup
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Old Library file from 2007/2010 release.  
-#I don't think we need this because we no longer explicitly check site locks, and I moved the
-#"Get-FilenameFromPath function into this file.
-
-# Path to scripts
-#$SCRIPTHOME         = & { split-path $myInvocation.ScriptName }
-#$LIBRARY_SPBACKUP   = join-path $SCRIPTHOME 'UVM-SharePoint.psm1'
-#
-# Check dependecies             
-#if ( -not (test-path $LIBRARY_SPBACKUP) ) {
-#    write-error "`nRequired file not found:`n$LIBRARY_SPBACKUP"
-#    exit
-#}
-
-# Source the library module
-#Import-Module $LIBRARY_SPBACKUP
-
-# Some timekeeping variables
+# timekeeping variables
 $timestamp_start = get-date
 $iso_date        = get-date -date $TIMESTAMP_Start -format yyyy-MM-ddTHHmm
 
@@ -102,18 +64,12 @@ $iso_date        = get-date -date $TIMESTAMP_Start -format yyyy-MM-ddTHHmm
 $logfile = '\_sharepoint_backup.' + $ISO_DATE + '.log'
 $logfile = join-path $destination $logfile
 
-if ( test-path $logfile ) {
+if (test-path $logfile) {
     remove-item $logfile
 }
 
-
-# Misc variables
-$limit = 0  # for testing, limit number of SPSites connected backed-up
-
 # Output helpers
-$blankline = ''
-$dashline  = '-' * 70
-$indent    = ' ' *  4
+[string]$blankline = ''
 
 # Create date against which SharePoint site modification will be compared
 if ($days -or $hours) {
@@ -129,16 +85,22 @@ else {
 #Load SharePoint PSSnapin
 Add-PSSnapin Microsoft.SharePoint.PowerShell
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#                      End Script configuration
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-# Function definitions
+# Begin Function definitions
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Tee-Log
 # emit messages to both log (appending) and screen
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function Tee-Log ($message ) {
+function Tee-Log {
+    param(
+        [string]$message,
+        [string]$logfile
+    )
     if ( $WhatIf.IsPresent ) {
         $message = 'What if: ' + $message
     }
@@ -151,7 +113,11 @@ function Tee-Log ($message ) {
 # Out-Log
 # emit messages to log (appending)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function Out-Log ($message ) {
+function Out-Log {
+    param(
+        [string]$message,
+        [string]$logfile
+    )
     if ( $WhatIf.IsPresent ) {
         $message = 'What if: ' + $message
     }
@@ -164,9 +130,11 @@ function Out-Log ($message ) {
 #   Simple set of strings to start the log
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 function Get-LogHeader {
-    "Sharepoint Site Backups beginning  ::  $timestamp_start"
-    $dashline
-    ""
+    [string]$dashline  = '-' * 70
+    Write-Output ""
+    Write-Output "Sharepoint Site Backups beginning  ::  $timestamp_start"
+    Write-output $dashline
+    Write-Output ""
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -175,10 +143,11 @@ function Get-LogHeader {
 #   Simple set of strings to start the log
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 function Get-LogFooter {
-    ""
-    "Sharepoint Site Backups finished  ::  $(get-date)"
-    $dashline
-    ""
+    [string]$dashline  = '-' * 70
+    Write-Output ""
+    Write-Output "Sharepoint Site Backups finished  ::  $(get-date)"
+    Write-Output $dashline
+    Write-Output ""
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -186,58 +155,60 @@ function Get-LogFooter {
 # given the path portion of the URL (from ServerRelativeUrl prop)
 # return a reasonable filename
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function Get-FilenameFromPath ($path) {
-
+function Get-FilenameFromPath {
+    param($path)
     # creating a regex pattern to match anything
     # that ISN'T a good (simple) filename character
 
     $notfilename  = [regex] '[<>:"/\\|?\* ]'
-	
+    
     $filename = ''
-	if ( $path -eq '/' ) {
-		$filename = '_sp_root'
-	}
-	else {
-		$filename = $path     -replace('/sites/' , '' )
-		$filename = $filename -replace( $notfilename , '_')
-	}
+    if ( $path -eq '/' ) {
+        $filename = '_sp_root'
+    }
+    else {
+        $filename = $path     -replace('/sites/' , '' )
+        $filename = $filename -replace( $notfilename , '_')
+    }
 
     $filename += '.spbak'
-	$filename.ToLower()
+    $filename.ToLower()
 }
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# End Function definitions
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Begin Main Routine
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-Tee-Log (Get-LogHeader)
-Tee-Log "Connecting to $siteurl"
-Tee-Log "Backup destination $destination"
+Get-LogHeader | % {Tee-Log $_ -logfile $logfile}
+Tee-Log "Connecting to $webApplication" -logfile $logfile
+Tee-Log "Backup destination $destination" -logfile $logfile
 
 if ( $afterdate ) {
-    Tee-Log "Backing up sites changed after $afterdate"
+    Tee-Log "Backing up sites changed after $afterdate" -logfile $logfile
 }
 
 # Connect to SharePoint WebApp
-$sharepoint = Get-SPWebApplication $siteurl
-if ( $sharepoint -eq $null ) {
-    throw "Unable to find SharePoint WebApp at $siteurl"
+$webApp = Get-SPWebApplication $webApplication
+if ( $webApp -eq $null ) {
+    throw "Unable to find SharePoint WebApp at $webApplication"
 }
 
 if ( $limit ) {
-    Tee-Log "Limit set to $limit sites"
+    Tee-Log "Limit set to $limit sites" -logfile $logfile
 }
-tee-log $blankline
+tee-log $blankline -logfile $logfile
 
 $sitecount  = 0;
 $errorcount = 0;
 
-:NEXTSITE foreach ($site in $sharepoint.sites) {
+:NEXTSITE foreach ($site in $webApp.sites) {
 
     # Stop if limit has been reached
-    if ( ( $limit -gt 0) -and ( $sitecount -gt $limit ) ) {
-        tee-log "Limit reached; ending"
+    if ( ($limit -gt 0) -and ($sitecount -gt $limit) ) {
+        tee-log "Limit reached; ending" -logfile $logfile
         $site.dispose()
         break
     }
@@ -247,9 +218,9 @@ $errorcount = 0;
 
     # Had some problems with "Ghost Sites"
     # if LastContentModified is Null, note Ghost Site and move on
-    if ( $site.LastContentModifiedDate -eq $null ) {
-        tee-log "   ( skipping $path , ghost site )"
-        tee-log $blankline
+    if ($site.LastContentModifiedDate -eq $null) {
+        tee-log "   ( skipping $path , ghost site )" -logfile $logfile
+        tee-log $blankline -logfile $logfile
         $site.dispose()
         continue NEXTSITE
     }
@@ -265,9 +236,9 @@ $errorcount = 0;
     }
 
     # if an interval was specified, skip sites not modified in interval
-    if ( $threshold -and ( $threshold -gt $modified ) ) {
-        tee-log "   ( skipping $path , modified $modified )"
-        tee-log $blankline
+    if ($threshold -and ($threshold -gt $modified) ) {
+        tee-log "   ( skipping $path , modified $modified )" -logfile $logfile
+        tee-log $blankline -logfile $logfile
         $site.dispose()
         continue NEXTSITE
     }
@@ -276,19 +247,19 @@ $errorcount = 0;
     $sitecount++
     $filename = Get-FilenameFromPath($path)
     #$size = [math]::round( ([long]$site.usage.storage/1MB), 2 )
-    tee-log "Backing-up $path"
-    out-log "    modified   $modified"
-    #out-log "    size (MB)  $size"
-    out-log "    filename   $filename"
+    tee-log "Backing-up $path" -logfile $logfile
+    out-log "    modified   $modified" -logfile $logfile
+    #out-log "    size (MB)  $size" -logfile $logfile
+    out-log "    filename   $filename" -logfile $logfile
 
     $filename = join-path $destination $filename
 
     # If the WhatIf parameter was specified, don't really 
     # lock or backup the SharePoint site collection
 
-    if ( $WhatIf.IsPresent ) {
-        tee-log "Performing backup operations for $path"
-        tee-log $blankline
+    if ($WhatIf.IsPresent) {
+        tee-log "Performing backup operations for $path" -logfile $logfile
+        tee-log $blankline -logfile $logfile
         $site.dispose()
         continue NEXTSITE
     }
@@ -296,32 +267,24 @@ $errorcount = 0;
 
     ## Backup the site
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     $url = $site.Url
-
     Write-Debug " >> Backing-up site $path`n"
-    #capture the error stream in the output
-    $errorActionPreference = 'continue'
-    $result = &STSADM -o Backup -url $url -filename $filename -overwrite 2>&1
-
-    # Get the next-to-last line from the output
-    if ( $result[-2] -ne 'Operation completed successfully.' ) {
-        tee-log "ERROR encountered in backup"
-        out-log $result[-2].TargetObject
-        write-warning $result[-2].TargetObject
-        tee-log $blankline
-        $site.dispose()
+    # STSADM is deprecated.  Use Backup-SPSite instead.
+    # (If not using SQL Server Enterprise Edition, you must exclude "-UseSqlSnapshot".)
+    try {
+        Backup-SPSite -Identity $url -Path $filename -Force -Confirm:$false -ea Stop #-UseSqlSnapshot
+    } catch {
+        tee-log "ERROR encountered in backup" -logfile $logfile
+        out-log $_.exception -logfile $logfile
+        write-warning $_.exception
+        tee-log $blankline -logfile $logfile
         $errorcount++
         continue NEXTSITE
+    } finally {
+        $site.dispose()
     }
-    else {
-        tee-log $result[-22]
-    }
-
-    ## Clean-up
-    tee-log $blankline
-
-    $site.Dispose()
+    tee-log "Backup of the site collection at $url completed successfully." -logfile $logfile
+    tee-log $blankline -logfile $logfile
 }
 
 # Report time to complete backup operation
@@ -332,5 +295,5 @@ $runtime  = $format -f ($duration.Days * 24 + $duration.Hours),
                  $duration.Minutes, 
                 ($duration.Seconds + $duration.Milliseconds/1000)
 
-Tee-Log "Backup opertion runtime: $runtime"
-Tee-Log (Get-LogFooter)
+Tee-Log "Backup opertion runtime: $runtime" -logfile $logfile
+Get-LogFooter | % {Tee-Log $_ -logfile $logfile}
