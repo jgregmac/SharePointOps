0c961e21b2443849ccdfbd2b05dfa73a58c2226f
jgregmac <jgreg@outlook.com>
2015-04-13 11:34:47 -0400

    mailto_siteAdmins.ps1 - refinements to bodyTemplate file, more console output for the script, added verbose output option, error trapping, and a half-hearted attempt to generalize the script with overrideable input parameters.

--- 2013/mailto_SiteAdmins.ps1
+++ 2013/mailto_SiteAdmins.ps1
@@ -5,49 +5,90 @@
 - Removes common "admininstrator" account prefixes/suffixes.
 - Sends the message defined in $bodyTemplate to the site owner. (Assumes the site UserLogon is a valid email when appending "@uvm.edu").
 #>
+[cmdletBinding()]
 param(
     [string]$waUrl = "https://spwinauth.uvm.edu",
     [string]$SmtpServer = "smtp.uvm.edu",
     [string]$From = "saa-ad@uvm.edu",
     [string]$subjTemplate = 'Pending Upgrade for your site "-siteURL-"',
-    [string]$templatePath = 'upgradeMailTemplate.html'
+    [string]$templateName = 'upgradeMailTemplate.html',
+    [string]$claimsPrefix = "i:0e.t|adfs.uvm.edu|",
+    [string]$filter,
+    [int]$limit
 )
 Set-PSDebug -Strict
+
+#Cast the output varaible as a string to avoid type confusion later:
+[string]$out = ''
+
 Add-PSSnapin -Name microsoft.SharePoint.PowerShell
 
-[string]$bodyTemplate = Get-Content -Path $templatePath
+Write-Host "Loading the message body template..." -ForegroundColor Cyan
+try {
+    $templatePath = Join-Path -Path $PSScriptRoot -ChildPath $templateName
+    [string]$bodyTemplate = Get-Content -Path $templatePath -ea stop
+} catch {
+    write-error "Could not read the message body template file."
+    write-error $_.exception
+    exit 100
+}
 
-$sites = Get-SPSite -WebApplication $waUrl -Limit All
+Write-Host "Gathering all site collections..." -ForegroundColor Cyan
+$sites = Get-SPSite -WebApplication $waUrl -Limit All -ea Stop -Verbose:$false
 $allAdmins = @()
 foreach ($site in $sites) {
-	#Write-Host "Working with site: " + $site.url
+    $out = ("  Working with site: " + $site.url)
+	Write-Verbose $out
 	$siteAdmins = @()
 	$siteAdmins = $site.RootWeb.SiteAdministrators
 	ForEach ($admin in $siteAdmins) {
-		#Write-Host "Adding Admin: " + $admin.UserLogin
-		[string]$a = $($admin.UserLogin).Replace("i:0e.t|adfs.uvm.edu|","")
-		[string]$a = $a.Replace("@campus.ad.","@")
-		[string]$a = $a.replace(".adm","")
-		[string]$a = $a.replace("-admin","")
-		[string]$a = $a.replace("admin-","")
-		if ($a -notmatch "sa_|\\system") { $allAdmins += , @($a; [string]$site.Url) }
+		$out =  "  Found site Admin: " + $admin.UserLogin
+        $out += "`r`n    Transforming the admin name to an email address..." 
+        Write-Verbose $out
+		[string]$a = $($admin.UserLogin).Replace($claimsPrefix,"")
+		$a = $a.replace("@campus.ad.","@")
+		$a = $a.replace(".adm","")
+		$a = $a.replace("-admin","")
+		$a = $a.replace("admin-","")
+        $a = $a.replace("-tech","")
+        #Convert the internal WebApp url to the user-facing, external Url:
+        [string]$siteUrl = $site.url.Replace($waUrl,'https://sharepoint2013.uvm.edu')
+        #Filter out system, service account, and windows-auth only users:
+		if ($a -notmatch "sa_|\\system|i:0#\.w") { $allAdmins += , @($a; $siteUrl) }
+        $out = "  Admin transformed to email address: " + $a
+        Write-Verbose $out
 	}
 	$site.Dispose()
 }
 
-#Production clause:
-#$allAdmins = $allAdmins | Sort-Object -Unique
-#During testing, I am filtering for one user (me) and limiting results to four sites:
-$allAdmins = $allAdmins | ? {$_[0] -match "jgm"} | Select-Object -Last 4
+Write-Host "Filtering results..." -ForegroundColor Cyan
+#Eliminate duplicates:
+$allAdmins = $allAdmins | Sort-Object -Unique
+#Additional filtering/limiting of results, if requested:
+if ($filter) {
+    $allAdmins = $allAdmins | ? {$_[0] -match $filter}
+}
+if ($limit) {
+    $allAdmins = $allAdmins | Select-Object -First $limit
+}
 
 foreach ($admin in $allAdmins) {
 	[string] $to = $admin[0]
+    #
 	[string] $siteUrl = $admin[1]
-	$siteUrl = $siteUrl.Replace('spwinauth.uvm.edu','sharepoint2013.uvm.edu')
 	[string] $body = $bodyTemplate.Replace("-siteURL-",$siteUrl)
+    #The subject line will reference the current site url, not the upgrade url.  Transform...
 	$siteUrl = $siteUrl.Replace('sharepoint2013.','sharepoint.')
 	[string] $subj = $subjTemplate.Replace("-siteURL-",$siteUrl)
-	Send-MailMessage -To $to -From $From -SmtpServer $SmtpServer -Subject $subj -BodyAsHtml $body
-	write-host "Sent to: " + $to + " from: " + $from + " subject: " + $subj
-	#write-host "body: " + $body
+    try {
+	    Send-MailMessage -To $to -From $From -SmtpServer $SmtpServer -Subject $subj -BodyAsHtml $body -ea stop
+    } catch {
+        write-error "Error sending mail message:"
+        write-error $_.exception
+        exit 200
+    }
+	$out = "  Sent mail to: " + $to + "`r`n  For site: `r`n    " + $siteUrl + "`r`n"
+    write-host $out -ForegroundColor Gray
+    #Add loop delay to prevent flooding:
+    Start-Sleep -Seconds 3
 }
